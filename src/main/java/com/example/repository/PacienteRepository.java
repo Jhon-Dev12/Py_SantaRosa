9a591c52d0fe618158c6f900954c4bea820163bd
Jhon-Dev12 <145687553+Jhon-Dev12@users.noreply.github.com>
2025-10-21 00:07:05 -0500

    Revert "Modifique los errores de compilacion"

--- src/main/java/com/example/repository/PacienteRepository.java
+++ src/main/java/com/example/repository/PacienteRepository.java
@@ -3,7 +3,7 @@ package com.example.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.entity.Paciente;
+import com.example.Entity.Paciente;
 
 @Repository
 public interface PacienteRepository extends JpaRepository<Paciente,Integer> {
